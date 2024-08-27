09f987ca57739f526fcc5560781b73c2ebb6883f
mayurkakde <kakdemayur5@gmail.com>
2024-08-27 23:37:36 +0530

    Initial commit

---
+++ database/seeders/UserstableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserstableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $imagePath = 'https://i.ibb.co/1sspJdY/Akik-Hossain.jpg';
+
+        User::create([
+            'name' => 'kakde',
+            'role' => 'Admin',
+            'email' => 'kakde5@example.com',
+            'password' => bcrypt('kakde5'),
+            'image' => $imagePath,
+        ]);
+
+         User::create([
+            'name' => 'Mayur kakde',
+            'role' => 'Admin',
+            'email' => 'akikhs00@gmail.com',
+            'password' => bcrypt('akik87'),
+            'image' => $imagePath,
+        ]);
+    }
+}
---
+++ resources/views/Frontend/partials/homeDashboard.blade.php
@@ -0,0 +1,116 @@
+@extends('Frontend.master')
+@section('content')
+
+
+<div class="item">
+    <div class="slider-img">
+        <img src="{{ asset('assests/image/service 9.jpg') }}" alt="">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="slider-captions">
+                    <h1 class="slider-title fw-bold text-white">Human Resources Management Service</h1>
+                    <p class="text-dark text-white">Empowering businesses through strategic talent solutions and
+                        seamless HR management.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- /.slider -->
+<!-- services -->
+
+<!-- /.services -->
+
+
+<!-- features-section -->
+<div class="space-medium">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="section-title">
+                    <h2>Why Choose Us</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- features-center -->
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="feature-center line">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-briefcase fa-xl"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h3>Industry Leadership</h3>
+                        <p> Pioneering innovation, setting new standards, and shaping our sector's future through
+                            leadership and progress.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- /.features-center -->
+            <!-- features-center -->
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="feature-center line">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-thumbs-up fa-xl"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h3>Mission and Values</h3>
+                        <p>Guided by our commitment to innovation and sustainability, our ethos upholds integrity,
+                            values, and inclusivity, ensuring ethical alignment in all endeavors.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- /.features-center -->
+            <!-- features-center -->
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="feature-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-people-group fa-xl"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h3>Employee Satisfaction</h3>
+                        <p> Our culture prioritizes growth, balance, and inclusivity, nurturing a devoted team for
+                            consistent excellence.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- /.features-center -->
+        </div>
+    </div>
+</div>
+</div>
+<div class="space-medium">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="section-title">
+                    <h2>We Succeed Because Our Clients Succeed</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- clients logo -->
+            @foreach ($clients as $item)
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="client-block">
+                    <div class="client-head">
+                        <a href="#" class="client-img"><img src="{{ url('/uploads//' . $item->client_image) }}"
+                                alt=""></a>
+                    </div>
+                    <div class="client-content">
+                        <h4><a href="#">{{ $item->client_name }}</a></h4>
+                        <p>{{ $item->details }}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <hr>
+    </div>
+</div>
+@endsection
